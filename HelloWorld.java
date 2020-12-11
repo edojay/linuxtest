09721f70ddcda9007ff60201b4d321e3d4e95531
spjvr <esk_08@hotmail.com>
2020-12-11 09:57:53 +0100

    removed comment 1

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,6 @@ public class HelloWorld {
 		System.out.println("Hello world");
 		System.out.println("comment from branch");
 		System.out.println("Add one more print");
-		//comment here!
 	}
 }
-
-//this is a comment
+//Test two for squash
